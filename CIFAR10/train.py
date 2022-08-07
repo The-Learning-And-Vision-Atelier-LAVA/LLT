ddc442aad3ab28e03c8167e43e093ced6adb13af
Longguang Wang <wanglongguang01@163.com>
2022-08-07 22:07:54 +0800

    Add files via upload

---
+++ CIFAR10/train.py
@@ -0,0 +1,267 @@
+import argparse
+import os
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+from models.resnet import *
+from models.vgg import *
+
+
+parser = argparse.ArgumentParser(description='PyTorch Cifar10 Training')
+parser.add_argument('--epochs', default=200, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet20')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int, metavar='N', help='mini-batch size (default: 128),only used for train')
+parser.add_argument('--lr', '--learning-rate', default=0.01, type=float, metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float, metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=100, type=int, metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true', help='evaluate model on validation set')
+parser.add_argument('-ct', '--cifar-type', default='10', type=int, metavar='CT', help='10 for cifar10,100 for cifar100 (default: 10)')
+parser.add_argument('-id', '--device', default='0', type=str, help='gpu device')
+parser.add_argument('--w_bits', default=2, type=int, help='the bit-width of the quantized network')
+parser.add_argument('--a_bits', default=2, type=int, help='the bit-width of the quantized network')
+
+best_prec = 0
+args = parser.parse_args()
+
+
+def main():
+    global args, best_prec
+    use_gpu = torch.cuda.is_available()
+    print(args.device)
+    print('=> Building model...')
+
+    if use_gpu:
+        if args.arch == 'resnet20':
+            model = resnet20_cifar(w_bits=args.w_bits, a_bits=args.a_bits)
+        elif args.arch == 'vgg':
+            model = VGG(w_bits=args.w_bits, a_bits=args.a_bits)
+        else:
+            print('Architecture not support!')
+            return
+
+        model = nn.DataParallel(model.cuda(), list(map(int, args.device.split(','))))
+        criterion = nn.CrossEntropyLoss().cuda()
+        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
+
+        # load pre_trained full-precision model
+        print("=> loading pre-trained model")
+        if args.arch == 'resnet20':
+            ckpt = torch.load('result/' + args.arch + '_32bit_best.pth')
+        if args.arch == 'vgg':
+            ckpt = torch.load('result/' + args.arch + '_32bit_best.pth')
+        model.load_state_dict(ckpt, strict=False)
+
+        cudnn.benchmark = True
+    else:
+        print('Cuda is not available!')
+        return
+
+    if not os.path.exists('result'):
+        os.makedirs('result')
+    fdir = 'result/'+str(args.arch) + '_w' + str(args.w_bits) + '_a' + str(args.a_bits)
+    if not os.path.exists(fdir):
+        os.makedirs(fdir)
+
+    print('=> loading cifar10 data...')
+    normalize = transforms.Normalize(mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262])
+    train_dataset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=True,
+        download=True,
+        transform=transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+    test_dataset = torchvision.datasets.CIFAR10(
+        root='./data',
+        train=False,
+        download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    testloader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False, num_workers=2)
+
+    for epoch in range(args.start_epoch, args.epochs):
+        # update learning rate
+        adjust_learning_rate(optimizer, epoch)
+
+        # train for one epoch
+        train(trainloader, model, criterion, optimizer, epoch)
+
+        # evaluate on test set
+        prec = validate(testloader, model, criterion)
+
+        # remember best precision and save checkpoint
+        torch.save({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+        }, os.path.join(fdir, 'epoch' + str(epoch+1) + '.pth'))
+
+        if prec >= best_prec:
+            best_prec = max(prec, best_prec)
+            torch.save({
+                'epoch': epoch + 1,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }, os.path.join(fdir, 'best.pth'))
+
+        print('best acc: {:1f}'.format(best_prec))
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def train(trainloader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    model.train()
+    end = time.time()
+
+    for i, (input, target) in enumerate(trainloader):
+        # update tau
+        tau = max(1 * (0.001 ** ((epoch * 392 + i) / 40 / 392)), 0.001)
+        for m in model.modules():
+            if hasattr(m, '_update_tau'):
+                m.tau = tau
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        input, target = input.cuda(), target.cuda()
+
+        # compute output
+        output = model(input)
+
+        # losses
+        loss = criterion(output, target)
+
+        # measure accuracy and record loss
+        prec = accuracy(output, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec.item(), input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), 5)
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec {top1.val:.3f}% ({top1.avg:.3f}%)'.format(
+                   epoch, i, len(trainloader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1))
+
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    with torch.no_grad():
+        for i, (input, target) in enumerate(val_loader):
+            input, target = input.cuda(), target.cuda()
+
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            prec = accuracy(output, target)[0]
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec.item(), input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec {top1.val:.3f}% ({top1.avg:.3f}%)'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1))
+
+    print(' * Prec {top1.avg:.3f}% '.format(top1=top1))
+
+    return top1.avg
+
+
+def save_checkpoint(state, fdir, epoch):
+    filepath = os.path.join(fdir, 'epoch' + str(epoch) + '.pth')
+    torch.save(state, filepath)
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """For resnet, the lr starts from 0.1, and is divided by 10 at 80 and 120 epochs"""
+    if args.arch == 'vgg':
+        adjust_list = [80, 160]
+    elif args.arch == 'resnet20':
+        adjust_list = [80, 120]
+    if epoch in adjust_list:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = param_group['lr'] * 0.1
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+if __name__=='__main__':
+    main()
